65e1bb604fe6a383bdbf5b6d53c4e0645e919766
SanketTops <sanket.tops@gmail.com>
2023-08-18 11:43:37 +0530

    Loops

---
+++ Module-1/for.py
@@ -0,0 +1,14 @@
+"""for i in "Python":
+    print(i)"""
+
+
+"""for i in range(2,11,2):
+    print(i)"""
+
+"""for i in range(10,0,-1):
+    print(i)"""
+
+
+#4 * 1 = 4
+#4 * 2 = 8
+#4 * 3 = 12
---
+++ Module-1/while.py
@@ -0,0 +1,14 @@
+"""i=2
+
+while i<=10:
+    #body (statement)
+    #print(i)
+    print("Sanket",i)
+    i+=2 #i=i+1"""
+
+
+i=10
+
+while i>=1:
+    print(i)
+    i-=1
