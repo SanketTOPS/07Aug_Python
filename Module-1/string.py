0039b753b279a61c288a46568e69e16b7e30d350
SanketTops <sanket.tops@gmail.com>
2023-08-21 11:27:35 +0530

    Loops + String + Data

---
+++ Module-1/string.py
@@ -0,0 +1,13 @@
+mystr="This is Python!"
+print(mystr)
+
+"""print(mystr[0])
+print(mystr[-1])
+print(mystr[0:4]) #range (str slice)"""
+#print(mystr[-7:-1])
+#print(mystr[:4])
+
+#print(len(mystr))
+
+#print(mystr.index("Python"))
+print(mystr.find('P'))
---
+++ Module-1/while_loop.py
@@ -0,0 +1,7 @@
+i=2
+
+while i<=10: #true
+    print(i) #statement
+    #i+=1
+    i+=2
+
