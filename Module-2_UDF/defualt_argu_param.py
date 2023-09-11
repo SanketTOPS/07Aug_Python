9c616b5e63d38be322cc7ebd9ff01af9567c83e1
SanketTops <sanket.tops@gmail.com>
2023-09-11 11:26:24 +0530

    UDF

---
+++ Module-2_UDF/defualt_argu_param.py
@@ -0,0 +1,14 @@
+def getdata(id,name,sub='Python',city='Rajkot'):
+    print("ID:",id)
+    print("Name:",name)
+    print("Subject:",sub)
+    print("City:",city)
+
+#getdata(1,'Nirav')
+
+#getdata(1,'Sanket','iOS') #positional arguments
+
+
+#getdata('Sanket','iOS',1) #positional arguments
+
+getdata(name='Sanket',sub='iOS',id=1) #keyword argumnets
---
+++ Module-2_UDF/func_creat.py
@@ -0,0 +1,16 @@
+#Defining a Functions
+
+def myfunc():
+    print("This is User define function!") #statmenet
+
+
+def getsum(a,b):
+    print("Sum:",a+b)
+
+
+#Calling a Function
+
+myfunc()
+getsum(23,66)
+
+
