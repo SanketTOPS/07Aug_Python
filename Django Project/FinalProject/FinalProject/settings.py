0ced2ce911dee6bb15aaa500536f730b72fcbf11
SanketTops <sanket.tops@gmail.com>
2023-12-26 12:49:56 +0530

    About+Contact

--- Django Project/FinalProject/FinalProject/settings.py
+++ Django Project/FinalProject/FinalProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-z)_u0qj4*haaqt376=ju*jj2lk0rje7nu6cy2vltnhxp$=2!tq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
